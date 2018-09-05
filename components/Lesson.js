b96739989c1f1b0aef623088fbc115055ae8c69f
Kia Wright <kiawright@admins-mbp.lan>
2018-09-05 17:05:05 -0500

    add list of lessons to education page; link lessons

---
+++ components/Lesson.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Button,
+  Image,
+  Keyboard,
+  Linking,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from 'react-native-vector-icons';
+
+import Colors from '../constants/Colors';
+import Styles from '../constants/Styles';
+
+class Lesson extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+   render() {
+     const lessonNumber = this.props.lessonNumber;
+
+     return (
+       <View>
+         <Text style={Styles.menuItem}>Lesson {lessonNumber}</Text>
+
+         <View>
+           <TouchableHighlight
+             style={styles.optionTooltip}
+             underlayColor={Colors.white}
+             onPress={() => Linking.openURL('http://adaptivenutrition.us/lesson' + this.props.lessonNumber)}>
+             <Text style={styles.lessonText}>View lesson</Text>
+           </TouchableHighlight>
+
+           <TouchableHighlight
+             style={styles.optionTooltip}
+             underlayColor={Colors.white}
+             onPress={() => {}}>
+             <Text style={styles.lessonText}>Take quiz</Text>
+           </TouchableHighlight>
+          </View>
+       </View>
+     );
+   }
+}
+
+export default Lesson;
+
+Lesson.propTypes = {
+};
+
+const styles = StyleSheet.create ({
+  lessonWrapper: {
+    marginBottom: 20
+  },
+  lessonText: {
+    fontSize: 20,
+    color: Colors.black
+  }
+});
--- screens/secure/EducationScreen.js
+++ screens/secure/EducationScreen.js
@@ -7,8 +7,7 @@ import AuthService from '../../services/AuthService';
 import Colors from '../../constants/Colors';
 import Styles from '../../constants/Styles';
 
-import ModalWindow from '../../components/ModalWindow';
-import MenuRow from '../../components/MenuRow';
+import Lesson from '../../components/Lesson';
 
 import {
   Button,
@@ -46,35 +45,24 @@ export default class LoginScreen extends React.Component {
 
   render() {
     const { navigate } = this.props.navigation;
+    const lessons = [1,2,3,4,5,6,7,8,9,10];
 
     return (
       <View style={Styles.body}>
-        <View style={Styles.title}>
-          <Image source={require('../../assets/an_logo.png')} style={{ width: 75, height: 75 }} />
+        <View style={Styles.header}>
+          <Image source={require('../../assets/an_logo.png')} style={{ width: 50, height: 50 }} />
         </View>
 
         <ScrollView style={Styles.content}>
-          <Text style={Styles.menuItem}>How Do I Use the App?</Text>
-          <Text style={Styles.menuItemSubText}>Take a tour</Text>
+          <Text style={[Styles.bigTitle, Styles.pageTitle]}>Lessons</Text>
 
-          <ModalWindow label="Biometric Settings" currentModal="BIOMETRIC_SETTINGS" data={this.state.client}  />
-          <Text style={Styles.menuItemSubText}>What we use to build your meal plan</Text>
-
-          <ModalWindow label="Foods to Avoid" currentModal="FOODS_TO_AVOID" />
-          <Text style={Styles.menuItemSubText}>Inflammatory foods</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('http://adaptivenutrition.us/member-toolkit')}>Member Toolkit</Text>
-          <Text style={Styles.menuItemSubText}>Meal prep, recipes, & more</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('http://adaptivenutrition.us/client-faq')}>Client FAQ</Text>
-          <Text style={Styles.menuItemSubText}>Answers to our most frequently asked questions</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('mailto:support@adaptivenutrition.us')}>Email Us</Text>
-          <Text style={Styles.menuItemSubText}>Have a question or comment? Let us know!</Text>
-
-          <ModalWindow label="About" currentModal="ABOUT" />
+          {lessons.map(i => <Lesson key={i} lessonNumber={i} />)}
         </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create ({
+
+});
