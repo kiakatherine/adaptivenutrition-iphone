b251ba232fcf769f5589d8759ffebb54af0e2a5c
Kia Wright <kiawright@admins-mbp.lan>
2018-09-07 18:15:06 -0500

    add dummy data to scoreboard

--- screens/secure/ScoreboardScreen.js
+++ screens/secure/ScoreboardScreen.js
@@ -8,8 +8,6 @@ import Colors from '../../constants/Colors';
 import Styles from '../../constants/Styles';
 
 import Header from '../../components/Header';
-import ModalWindow from '../../components/ModalWindow';
-import MenuRow from '../../components/MenuRow';
 
 import {
   Button,
@@ -37,41 +35,35 @@ export default class LoginScreen extends React.Component {
 
   componentWillMount() {
     var client = firebase.database().ref('clients/-L5KTqELYJEOv55oR8bF');
+    // var challengeGroupTeam = firebase.database().ref('challengeGroupTeams/-LK7UbdarUAEVW9H8C2W');
+    var challengeGroupTeams = firebase.database().ref('challengeGroupTeams');
 
     client.on('value', snapshot => {
       this.setState({
         client: snapshot.val()
       });
     });
+
+    challengeGroupTeams.on('value', snapshot => {
+      this.setState({
+        challengeGroupTeams: snapshot.val()
+      });
+    });
   }
 
   render() {
     const { navigate } = this.props.navigation;
+    const teamName = this.state.client ? this.state.client.challengeGroupTeam : null;
+    let teams = [1,2,3,4,5];
 
     return (
       <View style={Styles.body}>
         <Header />
 
         <ScrollView style={Styles.content}>
-          <Text style={Styles.menuItem}>How Do I Use the App?</Text>
-          <Text style={Styles.menuItemSubText}>Take a tour</Text>
+          <Text style={[Styles.bigTitle, Styles.pageTitle]}>Scoreboard</Text>
 
-          <ModalWindow label="Biometric Settings" currentModal="BIOMETRIC_SETTINGS" data={this.state.client}  />
-          <Text style={Styles.menuItemSubText}>What we use to build your meal plan</Text>
-
-          <ModalWindow label="Foods to Avoid" currentModal="FOODS_TO_AVOID" />
-          <Text style={Styles.menuItemSubText}>Inflammatory foods</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('http://adaptivenutrition.us/member-toolkit')}>Member Toolkit</Text>
-          <Text style={Styles.menuItemSubText}>Meal prep, recipes, & more</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('http://adaptivenutrition.us/client-faq')}>Client FAQ</Text>
-          <Text style={Styles.menuItemSubText}>Answers to our most frequently asked questions</Text>
-
-          <Text style={Styles.menuItem} onPress={() => Linking.openURL('mailto:support@adaptivenutrition.us')}>Email Us</Text>
-          <Text style={Styles.menuItemSubText}>Have a question or comment? Let us know!</Text>
-
-          <ModalWindow label="About" currentModal="ABOUT" />
+          {teams.map((team, i) => <Text key={i}>{team}</Text>)}
         </ScrollView>
       </View>
     );
