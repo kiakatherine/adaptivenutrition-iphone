5f99fd44e0f9edda9377a46a698929e4497829fe
Jamie Palmer <jp.development942@gmail.com>
2018-12-15 22:27:52 +0800

    no message

--- components/BodyweightGraph.js
+++ components/BodyweightGraph.js
@@ -208,7 +208,7 @@ const styles = StyleSheet.create ({
     marginBottom: 30,
     paddingTop: 10,
     paddingBottom: 10,
-    height: 300,
+    // height: 300,
     borderTopWidth: 1,
     borderBottomWidth: 1,
     borderColor: Colors.gray
