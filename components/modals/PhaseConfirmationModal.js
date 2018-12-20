fb5710861aaea8d3251fca96be88d1cf9345f819
Kia Wright <kiawright@admins-mbp-2.lan>
2018-12-20 16:08:34 -0600

    restyle nav phase modal

--- components/modals/PhaseConfirmationModal.js
+++ components/modals/PhaseConfirmationModal.js
@@ -45,46 +45,69 @@ class PhaseConfirmationModal extends React.Component {
         <Text style={Styles.modalParagraph}>{"But first, let's quickly ensure you're set up for success!"}</Text>
         <Text style={Styles.modalParagraph}>{"Check off the items below to confirm you're ready to move forward."}</Text>
         <Text style={Styles.modalParagraph}>{"If you're unsure about any of them, no worries. We recommend spending a few more days on the current phase nailing these down before moving on."}</Text>
+        <Text></Text>
 
         {this.props.currentPhase === 1 && <View>
-          <View style={[styles.checkboxRow, this.state.checkedPhaseOne1 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseOne1 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseOne1: !this.state.checkedPhaseOne1 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne1 ? styles.checkedText : styles.uncheckedText]}>I have stayed on Phase {this.props.currentPhase} for at least 1 week.</Text>
+              <Text>{this.state.checkedPhaseOne1 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne1 ? styles.checkedText : styles.uncheckedText]}>{"I've stayed on Phase "} {this.props.currentPhase} {" for at least 1 week."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseOne2 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseOne2 ? styles.checked : '']}
               onPress={ () => { this.setState({ checkedPhaseOne2: !this.state.checkedPhaseOne2 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne2 ? styles.checkedText : styles.uncheckedText]}>I have gotten all my meals in each day.</Text>
+              <Text>{this.state.checkedPhaseOne2 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne2 ? styles.checkedText : styles.uncheckedText]}>{"I've gotten all my meals in each day."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseOne3 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseOne3 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseOne3: !this.state.checkedPhaseOne3 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne3 ? styles.checkedText : styles.uncheckedText]}>I have eaten according to the food options in the app.</Text>
+              <Text>{this.state.checkedPhaseOne3 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne3 ? styles.checkedText : styles.uncheckedText]}>{"I've eaten according to the food options in the app."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseOne4 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseOne4 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseOne4: !this.state.checkedPhaseOne4 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne4 ? styles.checkedText : styles.uncheckedText]}>I have spaced my meals out according to the app.</Text>
+              <Text>{this.state.checkedPhaseOne4 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne4 ? styles.checkedText : styles.uncheckedText]}>{"I've spaced my meals out according to the app."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseOne5 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseOne5 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseOne5: !this.state.checkedPhaseOne5 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne5 ? styles.checkedText : styles.uncheckedText]}>I have eaten the portions according to the app.</Text>
+              <Text>{this.state.checkedPhaseOne5 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseOne5 ? styles.checkedText : styles.uncheckedText]}>{"I've eaten the portions according to the app."}</Text>
           </View>
+
+          <Text></Text>
 
           <TouchableHighlight
             style={[Styles.modalButton,
@@ -108,70 +131,89 @@ class PhaseConfirmationModal extends React.Component {
               this.state.checkedPhaseOne5 ? null : Styles.modalButtonDisabledText]}>
               {"I'M READY!"}</Text>
           </TouchableHighlight>
+
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
         </View>}
 
         {this.props.currentPhase === 2 && <View>
-          <View style={[styles.checkboxRow, this.state.checkedPhaseTwo1 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseTwo1 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseTwo1: !this.state.checkedPhaseTwo1 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo1 ? styles.checkedText : styles.uncheckedText]}>I have stayed on Phase {this.props.currentPhase} for at least 1 week.</Text>
+              <Text>{this.state.checkedPhaseTwo1 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo1 ? styles.checkedText : styles.uncheckedText]}>{"I've stayed on Phase "} {this.props.currentPhase} {" for at least 1 week."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseTwo2 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseTwo2 ? styles.checked : '']}
               onPress={ () => { this.setState({ checkedPhaseTwo2: !this.state.checkedPhaseTwo2 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo2 ? styles.checkedText : styles.uncheckedText]}>I have gotten all my meals in each day.</Text>
+              <Text>{this.state.checkedPhaseTwo2 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo2 ? styles.checkedText : styles.uncheckedText]}>{"I've gotten all my meals in each day."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseTwo3 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseTwo3 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseTwo3: !this.state.checkedPhaseTwo3 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo3 ? styles.checkedText : styles.uncheckedText]}>I have eaten according to the food options in the app.</Text>
+              <Text>{this.state.checkedPhaseTwo3 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo3 ? styles.checkedText : styles.uncheckedText]}>{"I've eaten according to the food options in the app."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseTwo4 ? styles.checked : '']}>
+          <View style={styles.checkboxRow}>
             <TouchableHighlight
+              style={[styles.checkbox, this.state.checkedPhaseTwo4 ? styles.checked : '']}
               underlayColor={Colors.secondaryColor}
               onPress={ () => { this.setState({ checkedPhaseTwo4: !this.state.checkedPhaseTwo4 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo4 ? styles.checkedText : styles.uncheckedText]}>I have spaced my meals out according to the app.</Text>
+              <Text>{this.state.checkedPhaseTwo4 && <FontAwesome
+                name='check'
+                size={20} />}</Text>
             </TouchableHighlight>
+            <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo4 ? styles.checkedText : styles.uncheckedText]}>{"I've spaced my meals out according to the app."}</Text>
           </View>
 
-          <View style={[styles.checkboxRow, this.state.checkedPhaseTwo5 ? styles.checked : '']}>
-            <TouchableHighlight
-              underlayColor={Colors.secondaryColor}
-              onPress={ () => { this.setState({ checkedPhaseTwo5: !this.state.checkedPhaseTwo5 }); }}>
-              <Text style={[Styles.modalParagraph, this.state.checkedPhaseTwo5 ? styles.checkedText : styles.uncheckedText]}>I have weighed and measured my meals.</Text>
-            </TouchableHighlight>
-          </View>
+          <Text></Text>
 
           <TouchableHighlight
             style={[Styles.modalButton,
               this.state.checkedPhaseTwo1 &&
               this.state.checkedPhaseTwo2 &&
               this.state.checkedPhaseTwo3 &&
-              this.state.checkedPhaseTwo4 &&
-              this.state.checkedPhaseTwo5 ? null : Styles.modalButtonDisabled]}
+              this.state.checkedPhaseTwo4 ? null : Styles.modalButtonDisabled]}
             underlayColor={Colors.primaryColor}
             onPress={() => {
               this.state.checkedPhaseTwo1 &&
               this.state.checkedPhaseTwo2 &&
               this.state.checkedPhaseTwo3 &&
-              this.state.checkedPhaseTwo4 &&
-              this.state.checkedPhaseTwo5 ? this.props.movePhase(3) : null}}>
+              this.state.checkedPhaseTwo4 ? this.props.movePhase(3) : null}}>
             <Text style={[Styles.modalButtonText,
               this.state.checkedPhaseTwo1 &&
               this.state.checkedPhaseTwo2 &&
               this.state.checkedPhaseTwo3 &&
-              this.state.checkedPhaseTwo4 &&
-              this.state.checkedPhaseTwo5 ? null : Styles.modalButtonDisabledText]}>
+              this.state.checkedPhaseTwo4 ? null : Styles.modalButtonDisabledText]}>
               {"I'M READY!"}</Text>
           </TouchableHighlight>
+
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
         </View>}
       </View>
     );
@@ -183,17 +225,22 @@ export default PhaseConfirmationModal;
 const styles = StyleSheet.create({
   checkboxRow: {
     flexDirection: 'row',
-    marginBottom: 15,
-    padding: 12,
-    paddingBottom: 3
+    // marginBottom: 10,
+    paddingRight: 50
+  },
+  checkbox: {
+    backgroundColor: Colors.lightGray,
+    borderRadius: 5,
+    marginRight: 10,
+    width: 32,
+    height: 32,
+    padding: 5
   },
   checked: {
-    backgroundColor: Colors.secondaryColor,
-    borderRadius: 5
+    backgroundColor: Colors.primaryColor
   },
   checkedText: {
-    color: Colors.white,
-    fontWeight: 'bold'
+    color: Colors.primaryColor
   },
   uncheckedText: {
     color: Colors.black
